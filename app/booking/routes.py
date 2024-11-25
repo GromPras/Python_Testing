e2c984675261785dc80166ae1996e611d0acc7b9
Jérôme Peyras <jerome.peyras@protonmail.com>
2024-11-25 11:05:58 +0100

    Finish booking bp setup

--- app/booking/routes.py
+++ app/booking/routes.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import render_template, request, flash
 from app.booking import bp
 
 
